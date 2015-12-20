6e4eedab30a32d2df5e72970793ac64e20f8512e
Justin Schulz <justin.schulz@gmail.com>
2015-12-20 19:32:58 +0000

    Trying to make login with the facebooks work.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
   
-  # def self.from_omniauth(auth)
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #     user.email = auth.info.email
-  #     user.password = Devise.friendly_token[0,20]
-  #   end
-  # end
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
   
   # def self.new_with_session(params, session)
   #   super.tap do |user|
