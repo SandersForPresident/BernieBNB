62b450c2f042297d62fa545bb4acd93f9a55c890
Justin Schulz <justin.schulz@gmail.com>
2015-12-29 16:44:43 -0500

    Set up app for sendgrid.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.log_level = :debug
+  config.action_mailer.default_url_options = { :host => "www.heroku.com" }
 end
