75920a1c757651a099a065eda112458c4cad8889
Justin Schulz <justin.schulz@gmail.com>
2015-12-29 17:18:50 -0500

    Allow users to update email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :phone)
+    params.require(:user).permit(:first_name, :phone, :email)
   end
 end
