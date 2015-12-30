82268ca2c2ec8ae8a1c9941fe49c5d8dd3353378
Justin Schulz <justin.schulz@gmail.com>
2015-12-30 13:05:49 -0500

    Set up google auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
+    scope: ['email', 'https://www.googleapis.com/auth/gmail.modify'],
+    access_type: 'offline'
+  }
 end
