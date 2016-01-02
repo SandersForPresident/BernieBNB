ec04a76026fd3ba0f10707a798b7a8b6703dcd57
Justin Schulz <justin.schulz@gmail.com>
2016-01-02 14:21:11 -0500

    Update contact email

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -3,8 +3,4 @@ class UserDecorator < Draper::Decorator
 
   decorates_association :visits
   decorates_association :hostings
-
-  def formatted_phone
-    "#{phone[0..2]}-#{phone[3..5]}-#{phone[5..9]}"
-  end
 end
