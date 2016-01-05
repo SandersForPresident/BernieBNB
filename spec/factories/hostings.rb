9c4f9a181374b5ddbb4d25a6415b30b5799cb76f
Al Snow <jasnow@hotmail.com>
2016-01-05 12:35:43 -0500

    Increased host/travelr max value from 7 to 10

--- spec/factories/hostings.rb
+++ spec/factories/hostings.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :hosting do |f|
     f.host_id { rand(1..5) }
     f.zipcode { Faker::Address.zip }
-    f.max_guests { rand(1..5) }
+    f.max_guests { rand(1..10) }
   end
 
 end
