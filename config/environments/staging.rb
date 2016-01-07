bc116dfcacb2742bdc951d3600b13ad5e7e3c760
Justin Schulz <justin.schulz@gmail.com>
2016-01-07 13:29:43 -0500

    Turn mailer url into env variable

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.log_level = :debug
-  config.action_mailer.default_url_options = { :host => "bbnb-staging.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => ENV['MAILER_URL'] }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.perform_deliveries = true
