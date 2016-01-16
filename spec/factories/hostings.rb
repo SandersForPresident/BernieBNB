5b1516c87a04ef20b7f27561811953e8ee2b7124
Justin Schulz <justin.schulz@gmail.com>
2016-01-16 17:33:45 -0500

    Finish adding comment to hostings

--- spec/factories/hostings.rb
+++ spec/factories/hostings.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :hosting do |f|
     f.host_id { rand(1..5) }
     f.zipcode { 11211 }
-    f.comment { Faker::Lorem.paragraph(1) }
+    f.comment { Faker::Lorem.paragraph(1)[0...140] }
     f.max_guests { rand(1..10) }
   end
 
