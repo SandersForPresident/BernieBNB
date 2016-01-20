6be747cc1605a485ce77014fc6847df2aee9d8b1
Justin Schulz <justin.schulz@gmail.com>
2016-01-20 18:11:31 -0500

    Allow users to see privacy policy when not signed in

--- app/controllers/policies_controller.rb
+++ app/controllers/policies_controller.rb
@@ -1,4 +1,7 @@
 class PoliciesController < ApplicationController
+  skip_before_action :require_current_user
+  skip_before_action :require_complete_profile
+
   def facebook
     render text: File.readlines(
       File.expand_path("../../../doc/bernie-bnb-facebook-privacy-statement.txt", __FILE__)
