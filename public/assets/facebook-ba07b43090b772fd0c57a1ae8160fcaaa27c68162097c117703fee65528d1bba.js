dab720f518dad46be994aa1795acd765b0b6dfdd
Tess Baklor <cockroachbill@gmail.com>
2016-02-09 15:14:17 -0600

    precompile assets

---
+++ public/assets/facebook-ba07b43090b772fd0c57a1ae8160fcaaa27c68162097c117703fee65528d1bba.js
@@ -0,0 +1,1 @@
+window.fbAsyncInit=function(){FB.init({appId:"209976379353747",xfbml:!0,version:"v2.5"})},function(e,n,t){var o,c=e.getElementsByTagName(n)[0];e.getElementById(t)||(o=e.createElement(n),o.id=t,o.src="//connect.facebook.net/en_US/sdk.js",c.parentNode.insertBefore(o,c))}(document,"script","facebook-jssdk");
