153358f5c67cd6b32ae922f301d58c02ddc7e487
Justin Schulz <justin.schulz@gmail.com>
2016-02-10 18:24:58 -0500

    Match visit dates on hosting maximum guests

--- app/decorators/hosting_decorator.rb
+++ app/decorators/hosting_decorator.rb
@@ -1,9 +1,11 @@
 class HostingDecorator < Draper::Decorator
   delegate_all
 
-  def short_details
-    location = city? && state? ? "#{city}, #{state.to_s}" : zipcode
-    guests = max_guests > 1 ? " (#{max_guests} guests)" : " (#{max_guests} guest)"
-    location << guests
+  def location
+    city? && state? ? "#{city}, #{state.to_s}" : zipcode
+  end
+
+  def guest_details
+    max_guests > 1 ? " #{max_guests} guests" : " #{max_guests} guest"
   end
 end
