37519fb19a01655dc62cebd6e33330c88d6f4376
Tess Baklor <cockroachbill@gmail.com>
2016-02-09 23:44:46 -0600

    put visit date below location in button

--- app/decorators/visit_decorator.rb
+++ app/decorators/visit_decorator.rb
@@ -2,6 +2,14 @@ class VisitDecorator < Draper::Decorator
   decorates :visit
   delegate_all
 
+  def date
+    date = "(#{start_and_end_dates})"
+  end
+
+  def location
+    location = city? && state? ? "#{city}, #{state.to_s}" : zipcode
+  end
+
   def short_details
     details = city? && state? ? "#{city}, #{state.to_s}" : zipcode
     details << " (#{start_and_end_dates})"
